38e82c5b84baa3a7dac0d1af252e084a113355d8
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2024-11-29 13:14:36 +0530

    Time: 1051 ms (44.12%), Space: 7.8 MB (5.29%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int i,sum=0;
+        for(i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        if(sum==num)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+        }
+};
