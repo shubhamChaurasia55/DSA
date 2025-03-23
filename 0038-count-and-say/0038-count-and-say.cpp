d2f15c2777708b67de40a19f691ad67ec0ec8236
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-03-23 12:42:52 +0530

    Time: 3 ms (81.04%), Space: 10.2 MB (42.81%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-    void solve(string &s,string temp, int n){
-        if(n==1) return;
-        int i=0;int j=0;
-        while(j<s.size()){
-            if(s[i]==s[j]) j++;
-            else{
-                char ch = '0'+(j-i);
-                temp = temp + ch + s[i];
-                i=j;
-                j++;
+    // void solve(string &s,string temp, int n){
+    //     if(n==1) return;
+    //     int i=0;int j=0;
+    //     while(j<s.size()){
+    //         if(s[i]==s[j]) j++;
+    //         else{
+    //             char ch = '0'+(j-i);
+    //             temp = temp + ch + s[i];
+    //             i=j;
+    //             j++;
+    //         }
+    //         if(j==s.size()){
+    //             char ch = '0'+(j-i);
+    //             temp = temp + ch + s[i];
+    //         }
+    //     }
+    //     s = temp;
+    //     solve(s,"",n-1);
+    // }
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        string temp = countAndSay(n-1);
+        string ans = "";
+        int freq = 1;
+        char ch = temp[0];
+        for(int i=1;i<temp.size();i++){
+            char dh = temp[i];
+            if(ch == dh){
+                freq++;
             }
-            if(j==s.size()){
-                char ch = '0'+(j-i);
-                temp = temp + ch + s[i];
+            else{ // ch != dh
+                ans += (to_string(freq) + ch);
+                freq = 1;
+                ch = dh;
             }
         }
-        s = temp;
-        solve(s,"",n-1);
-    }
-    string countAndSay(int n) {
-        string s = "1";
-        solve(s,"",n);
-        return s;
+        ans += (to_string(freq) + ch);
+        return ans;
     }
 };
