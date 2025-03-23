7bd4e05e9750425db13c7e1f91f95fdb0aa7afe4
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-03-23 20:12:46 +0530

    Time: 0 ms (100%), Space: 9 MB (14.04%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void solve(string s,string &temp, vector<string> &ans){
-        if(s==""){
-            ans.push_back(temp);
-            return;
+    string solve(string str,int k,string ans){
+        int n = str.size();
+        if(n==1){
+            ans += str;
+            return ans;
         }
-        for(int i= 0;i<s.size();i++){
-            temp.push_back(s[i]);
-            // s = s.substr(0,i) + s.substr(i+1);
-            solve((s.substr(0,i) + s.substr(i+1)),temp,ans);
-            temp.pop_back();
-        }
+        int fact = 1;
+        for(int i=2;i<=n-1;i++) fact *= i;
+
+        int idx = k/fact;
+        if(k%fact==0) idx--;
+        char ch = str[idx];
+        int q = 1;
+        if(k%fact==0) q=fact;
+        else q = k%fact;
+        return solve(str.substr(0,idx)+str.substr(idx+1),q,ans+ch);
     }
     string getPermutation(int n, int k) {
-        string s = "";
-        string temp = "";
-        for(int i=1;i<=n;i++){
-            s += to_string(i);
-        }
-        vector<string> ans;
-        solve(s,temp,ans);
-        return ans[k-1];
-        
+        string str = "";
+        for(int i=1;i<=n;i++) str += to_string(i);
+        return solve(str,k,"");
     }
 };
