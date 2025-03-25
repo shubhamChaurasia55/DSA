d34f9172b32801863d8d84ed19c561ca858b174c
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-03-26 00:17:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>> ans;
+    
+        for(int i=0;i<intervals.size()-1;i++){
+            if(intervals[i][1]<intervals[i+1][0]) ans.push_back(intervals[i]);
+            
+            else if(intervals[i][1]>intervals[i+1][1]){
+                intervals[i+1][1] = intervals[i][1];
+            }
+            else{
+                intervals[i+1][0] = intervals[i][0];
+            }
+        }
+        ans.push_back(intervals[intervals.size()-1]);
+        return ans;
+    }
+};
