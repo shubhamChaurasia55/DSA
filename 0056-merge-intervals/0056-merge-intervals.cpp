40a4585eef08e8edda3b0e572c0bc581364b5617
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-03-26 00:18:06 +0530

    Time: 3 ms (89.75%), Space: 23.7 MB (80.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,12 +7,11 @@ public:
     
         for(int i=0;i<intervals.size()-1;i++){
             if(intervals[i][1]<intervals[i+1][0]) ans.push_back(intervals[i]);
-            
-            else if(intervals[i][1]>intervals[i+1][1]){
-                intervals[i+1][1] = intervals[i][1];
-            }
             else{
                 intervals[i+1][0] = intervals[i][0];
+                if(intervals[i][1]>intervals[i+1][1])
+                    intervals[i+1][1] = intervals[i][1];
+            
             }
         }
         ans.push_back(intervals[intervals.size()-1]);
